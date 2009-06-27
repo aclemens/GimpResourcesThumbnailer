c0e7d4463d7c217c319bd7920a48970a249ce697
Andi Clemens <andi.clemens@gmx.net>
2009-06-27 12:26:48 +0200

    add missing VBR generator base class

---
+++ src/vbrgenerator.h
@@ -0,0 +1,31 @@
+/* ============================================================
+ *
+ * Date        : 2009-06-26
+ * Description : a VBR brush generator
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ============================================================ */
+
+#ifndef VBRGENERATOR_H
+#define VBRGENERATOR_H
+
+class VBRGenerator
+{
+public:
+
+    VBRGenerator();
+    ~VBRGenerator();
+};
+
+#endif /* VBRGENERATOR_H */
