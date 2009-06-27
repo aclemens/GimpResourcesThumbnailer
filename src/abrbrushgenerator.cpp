c74bceb9edae6e996ada5b2b6fa12571a273ff12
Andi Clemens <andi.clemens@gmx.net>
2009-06-27 12:26:22 +0200

    add ABG generator base class, none functional at the moment

---
+++ src/abrbrushgenerator.cpp
@@ -0,0 +1,38 @@
+/* ============================================================
+ *
+ * Date        : 2009-06-27
+ * Description : a generator for photoshop brushes
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ============================================================ */
+
+#include "abrbrushgenerator.h"
+
+// Qt includes
+
+#include <QString>
+
+ABRBrushGenerator::ABRBrushGenerator()
+{
+}
+
+ABRBrushGenerator::~ABRBrushGenerator()
+{
+}
+
+bool ABRBrushGenerator::load(const QString& filename)
+{
+    Q_UNUSED(filename)
+    return false;
+}
---
+++ src/abrbrushgenerator.h
@@ -0,0 +1,35 @@
+/* ============================================================
+ *
+ * Date        : 2009-06-27
+ * Description : a generator for photoshop brushes
+ *
+ * Copyright (C) 2009 by Andi Clemens <andi dot clemens at gmx dot net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ============================================================ */
+
+#ifndef ABRBRUSHGENERATOR_H
+#define ABRBRUSHGENERATOR_H
+
+class QString;
+
+class ABRBrushGenerator
+{
+public:
+
+    ABRBrushGenerator();
+    ~ABRBrushGenerator();
+
+    bool load(const QString& filename);
+};
+
+#endif /* ABRBRUSHGENERATOR_H */
